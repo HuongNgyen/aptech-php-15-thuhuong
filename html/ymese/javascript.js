734edd3a2470749701565c366eb8fecf06da16cd
HuongNgyen <huong123.g@gmail.com>
2019-06-04 22:15:08 +0700

    YMESE-NguyenThiThuHuong

---
+++ html/ymese/javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#show').hide();
+    $('#anvahien').click(function(){
+        $('#show').toggle();
+    });
+
+    $('#show1').hide();
+    $('#anvahien1').click(function(){
+        $('#show1').toggle();
+    });
+    $('#show2').hide();
+    $('#anvahien2').click(function(){
+        $('#show2').toggle();
+    });
+    $('#show3').hide();
+    $('#anvahien3').click(function(){
+        $('#show3').toggle();
+    });
+});
+
